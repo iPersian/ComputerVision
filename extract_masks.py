7d75e5146564937d0c4a10639b5a019109f625d1
maotek <maoshengj@hotmail.com>
2025-03-21 01:08:55 +0100

    WIP

--- extract_masks.py
+++ extract_masks.py
@@ -20,7 +20,9 @@ if __name__ == "__main__":
             upper = np.array([50, 255, 255])
             imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
             mask = cv2.inRange(imgHSV, lower, upper)
-            mask_fil = cv2.bilateralFilter(cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR), 20, 121, 34)
-            new_path = dest_path / image.relative_to(image.parts[0])
+            mask_fil = cv2.bilateralFilter(mask, 20, 121, 34)
+            # Ensure mask remains binary by thresholding
+            _, binary_mask_fil = cv2.threshold(mask_fil, 127, 255, cv2.THRESH_BINARY)
+            new_path = (dest_path / image.relative_to(image.parts[0])).with_suffix(".png")
             new_path.parent.mkdir(parents=True, exist_ok=True)
-            cv2.imwrite(new_path.resolve(), mask_fil)
+            cv2.imwrite(new_path.resolve(), binary_mask_fil)
